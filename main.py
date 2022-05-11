539d1b014beb11c909e261d7c10ad56c62bed681
panedrone <sqldalmaker@gmail.com>
2022-05-11 20:40:59 +0300

    1.239

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
+import os
 import sys
 
-try:
-    from dal.data_store import DataStore
-finally:
-    print("just to avoid replacement while reformat :)")
+# try:
+#     from dal.data_store import DataStore
+# finally:
+#     print("just to avoid replacement while reformat :)")
+
+import django.db
+from django.apps import AppConfig
+
+
+class MyDjangoAppConfig(AppConfig):
+    # default_auto_field = 'django.db.models.BigAutoField'
+    default_auto_field = 'django.db.models.AutoField'
+    name = 'dal'  # python package containing generated django models
+
+
+# there should be "settings.py" in the project root
+# Google --> django settings.py location
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+
+# django.setup() should be called before importing/using generated django models -->
+# AppRegistryNotReady("Apps aren't loaded yet.")
+django.setup()
 
 from django.core.management import execute_from_command_line
 
