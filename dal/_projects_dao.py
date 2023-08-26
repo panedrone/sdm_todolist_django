05f1d60cc00d1abba50b9e46168862057de5ed4f
panedrone <a.merezkiy@gmail.com>
2023-08-26 12:28:57 +0300

    1.285.5

--- dal/_projects_dao.py
+++ dal/_projects_dao.py
@@ -4,7 +4,6 @@ https://sqldalmaker.sourceforge.net/
 """
 
 from dal.project import Project
-from dal.project_li import ProjectLi
 
 
 class _ProjectsDao:
@@ -37,25 +36,3 @@ class _ProjectsDao:
         :return: int (the number of affected rows)
         """
         return self.ds.delete_one(Project, {'p_id': p_id})
-
-    def get_projects(self):
-        """
-        :return: list[ProjectLi]
-        """
-        sql = """select p.*, 
-                (select count(*) from tasks where p_id=p.p_id) as p_tasks_count 
-                from projects p 
-                order by p.p_id"""
-
-        _res = []
-
-        def _map_cb(row):
-            _obj = ProjectLi()
-            _obj.p_id = row["p_id"]  # q <- q
-            _obj.p_name = row["p_name"]  # q <- q
-            _obj.p_tasks_count = row["p_tasks_count"]  # q <- q
-            _res.append(_obj)
-
-        self.ds.query_all_rows(sql, [], _map_cb)
-
-        return _res
